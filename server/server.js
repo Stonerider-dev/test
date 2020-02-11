bff81c8741a288021e3765acecf3703926230962
Stonerider-dev <siriy.bohdan@gmail.com>
2020-02-11 20:03:22 +0200

    feat: add server and images

---
+++ server/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/images', (req, res) => {
+  fs.readFile("server/images.json", 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.send(JSON.parse(data))
+    }
+  })
+});
+
+app.post('/api/images/:id', (req, res) => {
+  const id = req.params.id;
+  const rate = req.body.rate;
+  if (!rate) return;
+  
+  fs.readFile("server/images.json", 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+      return
+    }
+    const images = JSON.parse(data);
+    const newImages = images.map(i => {
+      if (i.id === id) {
+        return {...i, rate};
+      }
+      return i;
+    });
+    fs.writeFile("server/images.json", JSON.stringify(newImages), err => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.send('Ok');
+      }
+    })
+  })
+
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
