3d074a138b6fa334daa059c9a3eae734c9d5b159
Stonerider-dev <siriy.bohdan@gmail.com>
2020-02-12 19:21:19 +0200

    feat: add eslint

--- server/server.js
+++ server/server.js
@@ -9,44 +9,43 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/images', (req, res) => {
-  fs.readFile("server/images.json", 'utf8', (err, data) => {
+  fs.readFile('server/images.json', 'utf8', (err, data) => {
     if (err) {
       console.log(err);
       res.status(500).send(err);
     } else {
-      res.send(JSON.parse(data))
+      res.send(JSON.parse(data));
     }
-  })
+  });
 });
 
 app.post('/api/images/:id', (req, res) => {
-  const id = req.params.id;
-  const rate = req.body.rate;
+  const { id } = req.params;
+  const { rate } = req.body;
   if (!rate) return;
-  
-  fs.readFile("server/images.json", 'utf8', (err, data) => {
+
+  fs.readFile('server/images.json', 'utf8', (err, data) => {
     if (err) {
       console.log(err);
       res.status(500).send(err);
-      return
+      return;
     }
     const images = JSON.parse(data);
-    const newImages = images.map(i => {
+    const newImages = images.map((i) => {
       if (i.id === id) {
-        return {...i, rate};
+        return { ...i, rate };
       }
       return i;
     });
-    fs.writeFile("server/images.json", JSON.stringify(newImages), err => {
+    fs.writeFile('server/images.json', JSON.stringify(newImages), (err) => {
       if (err) {
         console.log(err);
         res.status(500).send(err);
       } else {
         res.send('Ok');
       }
-    })
-  })
-
+    });
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
