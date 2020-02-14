a2da3c97d2c0b9564ca8f3c61be2d4e7da9481af
Stonerider-dev <siriy.bohdan@gmail.com>
2020-02-14 11:46:59 +0200

    feat: add date check, add description for images

--- src/components/CardList/components/Card/index.jsx
+++ src/components/CardList/components/Card/index.jsx
@@ -3,14 +3,20 @@ import PropTypes from 'prop-types';
 import { parseISO, format } from 'date-fns';
 import styles from './styles.module.css';
 
-const ISOToDate = date => format(parseISO(date), 'dd.MM.yyyy');
+const ISOToDate = date => {
+  try {
+    return format(parseISO(date), 'dd.MM.yyyy');
+  } catch (error) {
+    return error;
+  }
+};
 
 const Card = ({ onClick, url, rate, title, date }) => (
   <div className={styles.card}>
     <div className={styles.img_container} onClick={onClick} role="presentation">
       <img src={url} alt={title} className={styles.img} />
     </div>
-    <div className={styles.footer}>
+    <div>
       <div>{`Name: ${title}`}</div>
       <div>{`Date of adding: ${ISOToDate(date)}`}</div>
       <div>{`Rate: ${rate}`}</div>
